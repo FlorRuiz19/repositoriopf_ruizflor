78a2f852b5e2cdeef3e11c9c3e3a3f37886700e8
André <Andr@LAPTOP-J6DHBQKC>
2025-02-12 22:42:48 -0500

    Cargar archivos

---
+++ src/Programapf.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+public class Programapf {
+     public static void main(String[] args){
+        // declarar variable
+        String vendedor;
+        Double v1,v2,v3,sm,cm,sn;
+        Scanner lectura=new Scanner(System.in);
+        //entrada de datos
+        System.out.println("Nombre de vendedor");
+        vendedor=lectura.next();
+        System.out.println("Ventas 1");
+        v1=lectura.nextDouble();
+        System.out.println("Ventas 2");
+        v2=lectura.nextDouble();
+        System.out.println("Ventas 3");
+        v3=lectura.nextDouble();
+        System.out.println("Sueldo mensual");
+        sm=lectura.nextDouble();
+        //proceso de datos
+        cm=(v1+v2+v3)*0.1;
+        sn=sm+cm;
+        //salidad de datos
+        System.out.println("La comision de ventas es:"+cm);
+        System.out.println("El sueldo neto del vendedor es:"+sn);
+     }
+}
